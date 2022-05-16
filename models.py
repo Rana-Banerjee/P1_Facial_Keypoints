812a74e170071b28db82a7f5e066f16d9ed5a736
Rana-Banerjee <rana1224@gmail.com>
2022-05-16 12:06:24 +0530

    First submission

--- models.py
+++ models.py
@@ -20,7 +20,19 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.maxpool = nn.MaxPool2d(2,2)
+        self.dropout = nn.Dropout(0.25)
+        self.flatten = nn.Flatten()
         self.conv1 = nn.Conv2d(1, 32, 5)
+        # self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        # self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        # self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        # self.bn4 = nn.BatchNorm2d(256)
+        self.dense1 = nn.Linear(12*12*256, 128)
+        self.dense2 = nn.Linear(128, 68*2)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -31,7 +43,30 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = self.maxpool(F.relu(self.conv1(x)))
+        # x = self.bn1(x)
+        # x = self.dropout(x)
+        x = self.maxpool(F.relu(self.conv2(x)))
+        # x = self.bn2(x)
+        # x = self.dropout(x)
+        x = self.maxpool(F.relu(self.conv3(x)))
+        # x = self.bn3(x)
+        # x = self.dropout(x)
+        x = self.maxpool(F.relu(self.conv4(x)))
+        # x = self.bn4(x)
+        # x = self.dropout(x)
+        x = self.flatten(x) 
+        x = F.relu(self.dense1(x))
+        # x = self.dropout(x)
+        x = self.dense2(x)
+        x = torch.reshape(x,(-1,68,2))
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+
+import numpy as np
+NN = Net()
+input_numpy = np.random.random((1,1,224,224))
+input_tensor = torch.from_numpy(input_numpy).float()
+output = NN(input_tensor)
+print(output)
